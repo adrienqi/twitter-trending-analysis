2fdecc21b602d6cdf6f05338962421a113001e82
adriencheese <adrien.qi304@gmail.com>
2023-06-09 02:39:16 -0400

    Bugfix: create new badge elements for each trending topic instead of just one

--- scripts/content.js
+++ scripts/content.js
@@ -1,16 +1,18 @@
-const badge = document.createElement("p");
-badge.classList.add("color-secondary-text", "type--caption");
-badge.textContent = "testing";
-
 const timer = setInterval(() => {
     const elements = document.querySelectorAll(".css-901oao.r-1nao33i.r-37j5jr.r-a023e6.r-b88u0q.r-rjixqe.r-1bymd8e.r-bcqeeo.r-qvutc0");
     if (elements) {
         clearTimeout(timer);
         console.log(elements);
         elements.forEach(element => {
-            console.log(element.textContent)
-            console.log(element.insertAdjacentElement("beforeEnd", badge))
+            const badge = document.createElement("p");
+            badge.classList.add("color-secondary-text", "type--caption");
+            badge.textContent = "testing";
             
+            console.log(element.textContent)
+            const elementChild = element.childNodes[0]
+            console.log(elementChild)
+            // elementChild.insertAdjacentElement("beforeend", badge)
+            elementChild.appendChild(badge)
         })
     }
 }, 1000);
